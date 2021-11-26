42a72a3cb1e876492896593e42e760406001a7a8
onesmus muna <onesmusjones@gmail.com>
2021-11-26 09:36:50 +0300

    Connected PowerModule to main file

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  supplyPower(watts: number) {
+    console.log(`Suppling ${watts} worth of power`);
+  }
+}
